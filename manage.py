b366dd86cd2d28deb97f3f81e3269634bd52bea8
Jiangge Zhang <tonyseek@gmail.com>
2014-05-10 19:18:40 +0800

    add "clean" and "showurls" into manage.py

--- manage.py
+++ manage.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python
 
 from flask.ext.script import Manager
+from flask.ext.script.commands import Clean, ShowUrls
 
 from ailtdou.app import create_app
 
 
 app = create_app()
 manager = Manager(app)
+
+manager.add_command(Clean())
+manager.add_command(ShowUrls())
 
 
 if __name__ == '__main__':
