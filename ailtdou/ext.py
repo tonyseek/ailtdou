746a46594ade41ed95fc6ded90cae8c6567c521d
Jiangge Zhang <tonyseek@gmail.com>
2014-05-16 05:38:05 +0800

    upgrade to flask-oauthlib 0.5.0

--- ailtdou/ext.py
+++ ailtdou/ext.py
@@ -3,7 +3,11 @@ from contextlib import contextmanager
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.login import LoginManager
 from flask.ext.oauthlib.client import OAuth
+from flask.ext.oauthlib.contrib.apps import douban
 from raven.contrib.flask import Sentry
+
+
+__all__ = ['db', 'login_manager', 'oauth', 'sentry', 'capture_exception']
 
 
 db = SQLAlchemy()
@@ -11,16 +15,7 @@ login_manager = LoginManager()
 oauth = OAuth()
 sentry = Sentry()
 
-# registers OAuth app
-oauth.remote_app(
-    name='douban',
-    app_key='DOUBAN',
-    base_url='https://api.douban.com/v2/',
-    request_token_params={
-        'scope': 'douban_basic_common,shuo_basic_r,shuo_basic_w'},
-    access_token_url='https://www.douban.com/service/auth2/token',
-    authorize_url='https://www.douban.com/service/auth2/auth',
-    access_token_method='POST')
+douban.register_to(oauth)
 
 
 @contextmanager
