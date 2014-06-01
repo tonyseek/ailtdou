6a689b779d466f823c0c358a7cc3321825f99cf4
Jiangge Zhang <tonyseek@gmail.com>
2014-06-02 01:32:04 +0800

    migrate database and clean pyc while deploying.

--- fabfile.py
+++ fabfile.py
@@ -1,10 +1,13 @@
 from fabric.api import cd, run, get, put, sudo, hosts
+from fabric.context_managers import prefix
 
 
 remote_host = 'szulabs.org'
 remote_repo = '/srv/ailtdou.szulabs.org'
 remote_user = 'ailtdou'
 remote_proc = 'ailtdou:*'
+
+python_init = 'eval "$(pyenv init -)"'
 
 
 @hosts(remote_host)
@@ -18,8 +21,9 @@ def deploy():
     with cd(remote_repo):
         sudo('git pull --ff-only origin master', user=remote_user)
         run('pip install -r requirements.txt')
-        get('production/*.cfg', 'production/%(basename)s.cfg.last')
+        get('production/*.cfg', 'production/%(basename)s.last')
         put('production/*.cfg', 'production/', use_sudo=True)
-        with cd('production'):
-            sudo('honcho run ./manage.py db upgrade', user=remote_user)
+        with prefix(python_init), prefix('source production/.env'):
+            sudo('python manage.py clean', user=remote_user)
+            sudo('python manage.py db upgrade', user=remote_user)
     sudo('supervisorctl restart %s' % remote_proc)
