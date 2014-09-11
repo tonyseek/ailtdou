01b891459ec7e689ab4bbe53f6672e171c42fd11
Jiangge Zhang <tonyseek@gmail.com>
2014-09-11 18:40:45 +0800

    update the deployment script.

--- fabfile.py
+++ fabfile.py
@@ -1,14 +1,11 @@
 from fabric.api import cd, run, sudo, hosts
-from fabric.context_managers import prefix
 
 
 remote_host = 'szulabs.org'
 remote_repo = '/srv/ailtdou.szulabs.org'
 remote_user = 'ailtdou'
 remote_proc = 'ailtdou:*'
-
-python_init = 'eval "$(pyenv init -)"'
-honcho_init = 'honcho -e production/.env run '
+honcho_exec = 'pyenv exec honcho run '
 
 
 @hosts(remote_host)
@@ -16,7 +13,6 @@ def deploy():
     with cd(remote_repo):
         sudo('git pull --ff-only origin master', user=remote_user)
         run('pip install -r requirements.txt')
-        with prefix(python_init):
-            sudo(honcho_init + 'python manage.py clean', user=remote_user)
-            sudo(honcho_init + 'python manage.py db upgrade', user=remote_user)
+        sudo(honcho_exec + 'python manage.py clean', user=remote_user)
+        sudo(honcho_exec + 'python manage.py db upgrade', user=remote_user)
     sudo('supervisorctl restart %s' % remote_proc)
