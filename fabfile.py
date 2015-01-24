a24665d10417538b07905e4d313ebe32fafb4b04
Jiangge Zhang <tonyseek@gmail.com>
2015-01-24 19:27:26 +0800

    update deployment file.

--- fabfile.py
+++ fabfile.py
@@ -5,15 +5,15 @@ remote_host = 'szulabs.org'
 remote_repo = '/srv/ailtdou.szulabs.org'
 remote_user = 'ailtdou'
 remote_proc = ['ailtdou', 'ailtdou-smtp']
-honcho_exec = 'pyenv exec honcho run '
+envvar_exec = 'withenv pyenv exec '  # http://git.io/withenv
 
 
 @hosts(remote_host)
 def deploy():
     with cd(remote_repo):
         sudo('git pull --ff-only origin master', user=remote_user)
-        run('pip install -r requirements.txt')
-        sudo(honcho_exec + 'python manage.py clean', user=remote_user)
-        sudo(honcho_exec + 'python manage.py db upgrade', user=remote_user)
+        run(envvar_exec + 'pip install -r requirements.txt')
+        sudo(envvar_exec + 'python manage.py clean', user=remote_user)
+        sudo(envvar_exec + 'python manage.py db upgrade', user=remote_user)
     for proc in remote_proc:
         sudo('systemctl restart %s' % proc)
